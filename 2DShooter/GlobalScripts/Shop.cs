69a4fb6e3cb07a298b7fc2de10f597d202f91333
= <nalinbro28@gmail.com>
2020-07-20 20:18:31 +0530

    Implemented a basic Shop system

--- 2DShooter/GlobalScripts/Shop.cs
+++ 2DShooter/GlobalScripts/Shop.cs
@@ -3,13 +3,46 @@ using System;
 
 public class Shop : Node2D
 {
-    public override void _Draw()
+    public AnimationPlayer gunAnimPlayer;
+
+    private enum GunOnMenue
     {
-        
-    }
+        Pistol, Shotgun, RocketLauncher
+    };
+
+    GunOnMenue gunOnMenue;
 
     public override void _Ready()
     {
-        
+        gunAnimPlayer = GetNode<AnimationPlayer>("Guns/GunAnimPlayer");
+        gunOnMenue = GunOnMenue.Pistol;
+    }
+
+    private void OnRightSlideArrowPressed()
+    {
+        if (gunOnMenue == GunOnMenue.Pistol)
+        {
+            gunOnMenue = GunOnMenue.Shotgun;
+            gunAnimPlayer.Play("ShowShotgunFromLeft");
+        }
+        else if (gunOnMenue == GunOnMenue.Shotgun)
+        {
+            gunOnMenue = GunOnMenue.RocketLauncher;
+            gunAnimPlayer.Play("ShowRocketLauncher");
+        }
+    }
+
+    private void OnLeftSlideArrowPressed()
+    {
+        if (gunOnMenue == GunOnMenue.RocketLauncher)
+        {
+            gunOnMenue = GunOnMenue.Shotgun;
+            gunAnimPlayer.Play("ShowShotgunFromRight");
+        }
+        else if (gunOnMenue == GunOnMenue.Shotgun)
+        {
+            gunOnMenue = GunOnMenue.Pistol;
+            gunAnimPlayer.Play("ShowPistol");
+        }
     }
 }
