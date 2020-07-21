cf4a552e410b5253c5faad19cfd28b41a1242af7
= <nalinbro28@gmail.com>
2020-07-21 11:37:42 +0530

    Better Shopping Slot system

--- 2DShooter/GlobalScripts/Shop.cs
+++ 2DShooter/GlobalScripts/Shop.cs
@@ -5,44 +5,44 @@ public class Shop : Node2D
 {
     public AnimationPlayer gunAnimPlayer;
 
-    private enum GunOnMenue
+    private enum Slots
     {
-        Pistol, Shotgun, RocketLauncher
+        Slot0 = 0, Slot1 = -250, Slot2 = -500
     };
 
-    GunOnMenue gunOnMenue;
+    Slots currentSlot;
+    private Node2D guns;
 
     public override void _Ready()
     {
-        gunAnimPlayer = GetNode<AnimationPlayer>("Guns/GunAnimPlayer");
-        gunOnMenue = GunOnMenue.Pistol;
+        guns = GetNode<Node2D>("Slots");
+        currentSlot = Slots.Slot0;
+    }
+
+    public override void _Process(float delta)
+    {
+        MoveToSlot(currentSlot, delta, 6f);
     }
 
     private void OnRightSlideArrowPressed()
     {
-        if (gunOnMenue == GunOnMenue.Pistol)
-        {
-            gunOnMenue = GunOnMenue.Shotgun;
-            gunAnimPlayer.Play("ShowShotgunFromLeft");
-        }
-        else if (gunOnMenue == GunOnMenue.Shotgun)
-        {
-            gunOnMenue = GunOnMenue.RocketLauncher;
-            gunAnimPlayer.Play("ShowRocketLauncher");
-        }
+        if (currentSlot == Slots.Slot0)
+            currentSlot = Slots.Slot1;
+        else if (currentSlot == Slots.Slot1)
+            currentSlot = Slots.Slot2;
+
     }
 
     private void OnLeftSlideArrowPressed()
     {
-        if (gunOnMenue == GunOnMenue.RocketLauncher)
-        {
-            gunOnMenue = GunOnMenue.Shotgun;
-            gunAnimPlayer.Play("ShowShotgunFromRight");
-        }
-        else if (gunOnMenue == GunOnMenue.Shotgun)
-        {
-            gunOnMenue = GunOnMenue.Pistol;
-            gunAnimPlayer.Play("ShowPistol");
-        }
+        if (currentSlot == Slots.Slot2)
+            currentSlot = Slots.Slot1;
+        else if (currentSlot == Slots.Slot1)
+            currentSlot = Slots.Slot0;
+    }
+
+    private void MoveToSlot(Slots slots, float delta, float accel)
+    {
+        guns.GlobalPosition = guns.GlobalPosition.LinearInterpolate(new Vector2((float)slots, 0), delta * accel);
     }
 }
